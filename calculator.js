c84e8b6e6fa7d4c3ece2b465a821284029f078f8
Amir-Shrestha <amirshrestha.construct@gmail.com>
2021-11-10 10:59:16 +0545

    Clear all, Backslash, Click_Key feature add also calculate given expression using eval() method.

---
+++ calculator.js
@@ -0,0 +1,17 @@
+function click_key(val){
+    document.getElementById('input-view').innerHTML=document.getElementById('input-view').innerHTML+val;
+}
+
+function clear_all(){
+    document.getElementById('input-view').innerHTML='';
+    document.getElementById('result-view').innerHTML='';
+}
+
+function back_slash(){
+    document.getElementById('input-view').innerHTML=document.getElementById('input-view').innerHTML.slice(0, -1);
+}
+
+function result(){
+    expression = document.getElementById('input-view').innerHTML;
+    document.getElementById('result-view').innerHTML=eval(expression);
+}
